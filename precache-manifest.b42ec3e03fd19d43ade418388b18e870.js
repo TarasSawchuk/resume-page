467e471fc1b4d9f21ee98717a0b3c4c78a68da41
TarasSawchuk <taras338lm@gmail.com>
2019-12-23 22:34:11 +0200

    Updates

---
+++ precache-manifest.b42ec3e03fd19d43ade418388b18e870.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d774ce32b7354b9054054c87b62f11ae",
+    "url": "/resume-page/index.html"
+  },
+  {
+    "revision": "789e3930394fe9a10c26",
+    "url": "/resume-page/static/css/main.105e41ac.chunk.css"
+  },
+  {
+    "revision": "23edd04ca8ac8eeb1d68",
+    "url": "/resume-page/static/js/2.386253ea.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/resume-page/static/js/2.386253ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "789e3930394fe9a10c26",
+    "url": "/resume-page/static/js/main.77009d4e.chunk.js"
+  },
+  {
+    "revision": "8e4029f6c550d973d0b4",
+    "url": "/resume-page/static/js/runtime-main.f8dd42d1.js"
+  },
+  {
+    "revision": "3984cd6fdb745836b9a6dd3dcf71a7d4",
+    "url": "/resume-page/static/media/background-resume.3984cd6f.jpeg"
+  },
+  {
+    "revision": "5905efa8075a2f684f25cacd1093c110",
+    "url": "/resume-page/static/media/movie4evening.5905efa8.png"
+  },
+  {
+    "revision": "be057261e5cd5b009924bd98831d13df",
+    "url": "/resume-page/static/media/profile-photo.be057261.jpg"
+  },
+  {
+    "revision": "7a357ef86444e063f520b93b0d34fc48",
+    "url": "/resume-page/static/media/treking-gear.7a357ef8.png"
+  }
+]);
